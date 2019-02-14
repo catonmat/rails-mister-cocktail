b9dd2fa111ae5bfe299f43449e73ec6cdd6ebbe0
Richard Jarram <rjarram@me.com>
2019-02-14 13:34:08 -0500

    finished architecture

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    if @dose.save
+      redirect_to(cocktail_path(@cocktail))
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    dose = Dose.find(params[:id])
+    dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end

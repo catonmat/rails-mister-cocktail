2a5096a1c005b72dfaa86f3164b96cff539db3c4
Richard Jarram <rjarram@me.com>
2019-02-15 16:29:37 -0500

    heroku uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_224327) do
+ActiveRecord::Schema.define(version: 2019_02_15_210902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_224327) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "instructions"
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
